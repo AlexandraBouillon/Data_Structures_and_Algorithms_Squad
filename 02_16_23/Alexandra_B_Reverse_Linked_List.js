92fc1be438e3fe9c76b9ce09578d8d293e151f8a
Alexandra Bouillon <alexandra.bouillon@gmail.com>
2023-02-16 09:01:27 -0500

    adding in problem sets

---
+++ 02_16_23/Alexandra_B_Reverse_Linked_List.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function(head) {
+
+};
+
+console.log(reverseList([1,2,3,4,5])); //[5,4,3,2,1]
+console.log(reverseList([1,2])); // [2,1]
+console.log(reverseList([])); // []
