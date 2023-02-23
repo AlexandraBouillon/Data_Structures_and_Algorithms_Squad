3a8c3a2046a688f33b54bcb997506fc6e74c4bb2
Alexandra Bouillon <alexandra.bouillon@gmail.com>
2023-02-23 12:44:56 -0500

    adding in JS solution for Reverse Linked List

--- 02_16_23/Alexandra_B_Reverse_Linked_List.js
+++ 02_16_23/Alexandra_B_Reverse_Linked_List.js
@@ -1,18 +1,30 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-const reverseList = function(head) {
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+const reverseList = (head)=> {
+    let current = head
+    let prev = null
+    let next = null
 
+    while (current) {
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    }
+
+    return prev
 };
 
-console.log(reverseList([1,2,3,4,5])); //[5,4,3,2,1]
-console.log(reverseList([1,2])); // [2,1]
-console.log(reverseList([])); // []
+// const reverseList = function(head) {
+//     if (head === null) return null;
+//     if (head === []) return [];
+//     let body = head.slice(1)
+
+// };
+
+// console.log(reverseList([1,2,3,4,5])); //[5,4,3,2,1]
+console.log(reverseList([5,4,3,2,1])); //[5,4,3,2,1]
+// console.log(reverseList([1,2])); // [2,1]
+// console.log(reverseList([])); // []
