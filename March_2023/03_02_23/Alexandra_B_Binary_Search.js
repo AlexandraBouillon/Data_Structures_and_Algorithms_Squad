932f7f6ebcf48facda742966d91c8f6c1daa861e
Alexandra Bouillon <alexandra.bouillon@gmail.com>
2023-03-03 06:15:45 -0500

    adding in problem set solutions

---
+++ March_2023/03_02_23/Alexandra_B_Binary_Search.js
@@ -0,0 +1,18 @@
+const search = function(nums, target) { //[-1,0,3,5,9,12]
+    let left_index = 0; // 3
+    let right_index = nums.length-1; // 5
+    while (left_index <= right_index){ // 0 < 5
+        let mid_index = Math.floor((left_index + right_index)/2) // 2
+        // console.log(mid_index)
+        if (nums[mid_index] === target) { // 3 === 9
+            return mid_index; //
+        }
+        else if (nums[mid_index] > target){ // 3 > 9
+            right_index = mid_index-1;
+        } else {
+            left_index = mid_index+1 // 3
+        }
+    }
+    return -1;
+};
+console.log(search([-1,0,3,5,9,12]));
---
+++ March_2023/03_02_23/Alexandra_B_Is_Bad_Version.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 0;
+        let right = n;
+        let mid ;
+        while ( (right - left) > 1) {
+            mid = Math.floor((right + left)/2)
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else  {
+                left = mid;
+            }
+        }
+        return right;
+    };
+};
