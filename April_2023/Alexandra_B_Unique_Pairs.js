2d93d753a64b6c50b7e259273a2ed1dbe87b277c
Alexandra Bouillon <alexandra.bouillon@gmail.com>
2023-04-05 22:52:17 -0400

    adding in the problem set solutions

---
+++ April_2023/Alexandra_B_Unique_Pairs.js
@@ -0,0 +1,15 @@
+var uniquePaths = function(m, n) {
+    const unique_way = new Array(m).fill(1).map(() => new Array(n).fill(1));
+
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            let move_right = unique_way[i - 1][j]
+            let descend = unique_way[i][j - 1]
+            unique_way[i][j] = move_right + descend;
+        }
+    }
+
+    return unique_way[m - 1][n - 1];
+
+};
